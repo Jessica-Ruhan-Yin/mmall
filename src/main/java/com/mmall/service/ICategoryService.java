17b2c5c3076a2f044bfc6c7ffc5076fa23dea271
littlelittleJessica <446067382@qq.com>
2021-10-03 01:07:42 +0800

    7-3 增加查询平级分类接口方法

--- src/main/java/com/mmall/service/ICategoryService.java
+++ src/main/java/com/mmall/service/ICategoryService.java
@@ -1,6 +1,9 @@
 package com.mmall.service;
 
 import com.mmall.common.ServerResponse;
+import com.mmall.pojo.Category;
+
+import java.util.List;
 
 /**
  * @Description
@@ -13,4 +16,6 @@ public interface ICategoryService {
     ServerResponse addCategory(String categoryName, Integer parentId);
 
     ServerResponse updateCategoryName(Integer categoryId, String categoryName);
+
+    ServerResponse<List<Category>> getChildrenParallelCategory(Integer categoryId);
 }
--- src/main/java/com/mmall/service/Impl/CategoryServiceImpl.java
+++ src/main/java/com/mmall/service/Impl/CategoryServiceImpl.java
@@ -5,8 +5,13 @@ import com.mmall.dao.CategoryMapper;
 import com.mmall.pojo.Category;
 import com.mmall.service.ICategoryService;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
+import java.util.List;
 
 
 /**
@@ -18,14 +23,16 @@ import org.springframework.stereotype.Service;
 @Service("iCategoryService")
 public class CategoryServiceImpl implements ICategoryService {
 
+    private static final Logger LOG = LoggerFactory.getLogger(CategoryServiceImpl.class);
+
     @Autowired
     private CategoryMapper categoryMapper;
 
     /**
      * 添加分类
      */
-    public ServerResponse addCategory(String categoryName, Integer parentId){
-        if(parentId==null|| StringUtils.isBlank(categoryName)){
+    public ServerResponse addCategory(String categoryName, Integer parentId) {
+        if (parentId == null || StringUtils.isBlank(categoryName)) {
             return ServerResponse.createByErrorMessage("添加品类参数错误");
         }
 
@@ -35,7 +42,7 @@ public class CategoryServiceImpl implements ICategoryService {
         category.setStatus(true);
 
         int rowCount = categoryMapper.insert(category);
-        if(rowCount>0) {
+        if (rowCount > 0) {
             return ServerResponse.createBySuccess("添加品类成功");
         }
         return ServerResponse.createByErrorMessage("添加品类失败");
@@ -44,8 +51,8 @@ public class CategoryServiceImpl implements ICategoryService {
     /**
      * 更新分类名称
      */
-    public ServerResponse updateCategoryName(Integer categoryId, String categoryName){
-        if(categoryId==null||StringUtils.isBlank(categoryName)){
+    public ServerResponse updateCategoryName(Integer categoryId, String categoryName) {
+        if (categoryId == null || StringUtils.isBlank(categoryName)) {
             return ServerResponse.createByErrorMessage("更新品类的参数错误");
         }
         Category category = new Category();
@@ -53,10 +60,21 @@ public class CategoryServiceImpl implements ICategoryService {
         category.setName(categoryName);
 
         int rowCount = categoryMapper.updateByPrimaryKeySelective(category);
-        if(rowCount>0) {
+        if (rowCount > 0) {
             return ServerResponse.createBySuccess("更新品类名字成功");
         }
         return ServerResponse.createByErrorMessage("更新品类名字失败");
+    }
+
+    /**
+     * 获取平级categoryId
+     */
+    public ServerResponse<List<Category>> getChildrenParallelCategory(Integer categoryId) {
+        List<Category> categoryList = categoryMapper.selectCategoryChildrenByParentId(categoryId);
+        if (CollectionUtils.isEmpty(categoryList)) {
+            LOG.info("未找到当前分类的子分类");
+        }
+        return ServerResponse.createBySuccess(categoryList);
     }
 }
 

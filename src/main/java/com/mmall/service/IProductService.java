0ca2b31cbc6e170a9610aecc7f91997ede364fa0
littlelittleJessica <446067382@qq.com>
2021-10-03 14:53:11 +0800

    8-2 新增保存产品后端接口

---
+++ src/main/java/com/mmall/service/IProductService.java
@@ -0,0 +1,15 @@
+package com.mmall.service;
+
+import com.mmall.common.ServerResponse;
+import com.mmall.pojo.Product;
+
+/**
+ * @Description
+ * @Author Jessica
+ * @Version v
+ * @Date 2021/10/3
+ */
+public interface IProductService {
+
+    ServerResponse saveORUpdateProduct(Product product);
+}
---
+++ src/main/java/com/mmall/service/Impl/ProductService.java
@@ -0,0 +1,51 @@
+package com.mmall.service.Impl;
+
+import com.mmall.common.ServerResponse;
+import com.mmall.dao.ProductMapper;
+import com.mmall.pojo.Product;
+import com.mmall.service.IProductService;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * @Description
+ * @Author Jessica
+ * @Version v
+ * @Date 2021/10/3
+ */
+@Service("iProductService")
+public class ProductService implements IProductService {
+
+    @Autowired
+    private ProductMapper productMapper;
+
+
+    /**
+     * 保存产品
+     */
+    public ServerResponse saveORUpdateProduct(Product product) {
+        if (product != null) {
+            if (StringUtils.isNotBlank(product.getSubImages())) {
+                String[] subImageArray = product.getSubImages().split(",");
+                if (subImageArray.length > 0) {
+                    product.setMainImage(subImageArray[0]);
+                }
+            }
+            if (product.getId() != null) {
+                int rowCount = productMapper.updateByPrimaryKey(product);
+                if (rowCount > 0) {
+                    return ServerResponse.createBySuccessMessage("更新产品成功");
+                }
+                return ServerResponse.createByErrorMessage("更新产品失败");
+            } else {
+                int rowCount = productMapper.insert(product);
+                if (rowCount > 0) {
+                    return ServerResponse.createBySuccessMessage("更新产品成功");
+                }
+                return ServerResponse.createByErrorMessage("更新产品失败");
+            }
+        }
+        return ServerResponse.createByErrorMessage("新增或更新产品参数不正确");
+    }
+}

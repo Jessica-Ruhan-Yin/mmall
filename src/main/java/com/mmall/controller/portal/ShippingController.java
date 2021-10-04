2625bac677f24d27e846b4a98ebed24e079168be
littlelittleJessica <446067382@qq.com>
2021-10-05 00:36:45 +0800

    10-2 收货地址模块开发，增删改查及分页查询详情功能开发

---
+++ src/main/java/com/mmall/controller/portal/ShippingController.java
@@ -0,0 +1,98 @@
+package com.mmall.controller.portal;
+
+import com.github.pagehelper.PageInfo;
+import com.mmall.common.Const;
+import com.mmall.common.ResponseCode;
+import com.mmall.common.ServerResponse;
+import com.mmall.pojo.Shipping;
+import com.mmall.pojo.User;
+import com.mmall.service.IShippingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
+
+/**
+ * @Description
+ * @Author Jessica
+ * @Version v
+ * @Date 2021/10/4
+ */
+@Controller
+@RequestMapping("/shipping/")
+public class ShippingController {
+
+    @Autowired
+    private IShippingService iShippingService;
+
+    /**
+     * 增加收货地址
+     */
+    @RequestMapping("add.do")
+    @ResponseBody
+    public ServerResponse add(HttpSession session, Shipping shipping) {
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        if (user == null) {
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), ResponseCode.NEED_LOGIN.getDesc());
+        }
+        return iShippingService.add(user.getId(), shipping);
+    }
+
+    /**
+     * 删除收货地址
+     */
+    @RequestMapping("del.do")
+    @ResponseBody
+    public ServerResponse del(HttpSession session, Integer shippingId) {
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        if (user == null) {
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), ResponseCode.NEED_LOGIN.getDesc());
+        }
+        return iShippingService.del(user.getId(), shippingId);
+    }
+
+    /**
+     * 更新收货地址
+     */
+    @RequestMapping("update.do")
+    @ResponseBody
+    public ServerResponse update(HttpSession session, Shipping shipping) {
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        if (user == null) {
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), ResponseCode.NEED_LOGIN.getDesc());
+        }
+        return iShippingService.update(user.getId(), shipping);
+    }
+
+    /**
+     * 查询收货地址
+     */
+    @RequestMapping("select.do")
+    @ResponseBody
+    public ServerResponse select(HttpSession session, Integer shippingId) {
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        if (user == null) {
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), ResponseCode.NEED_LOGIN.getDesc());
+        }
+        return iShippingService.select(user.getId(), shippingId);
+    }
+
+    /**
+     * 分页查询
+     */
+    @RequestMapping("list.do")
+    @ResponseBody
+    public ServerResponse<PageInfo> list(@RequestParam(value = "pageNum", defaultValue = "1") int pageNum,
+                                         @RequestParam(value = "pageSize", defaultValue = "10") int pageSize,
+                                         HttpSession session) {
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        if (user == null) {
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), ResponseCode.NEED_LOGIN.getDesc());
+        }
+        return iShippingService.list(user.getId(), pageNum, pageSize);
+    }
+
+}
---
+++ src/main/java/com/mmall/service/IShippingService.java
@@ -0,0 +1,24 @@
+package com.mmall.service;
+
+import com.github.pagehelper.PageInfo;
+import com.mmall.common.ServerResponse;
+import com.mmall.pojo.Shipping;
+
+/**
+ * @Description
+ * @Author Jessica
+ * @Version v
+ * @Date 2021/10/4
+ */
+public interface IShippingService {
+
+    ServerResponse add(Integer userId, Shipping shipping);
+
+    ServerResponse del(Integer userId, Integer shippingId);
+
+    ServerResponse update(Integer userId, Shipping shipping);
+
+    ServerResponse select(Integer userId, Integer shippingId);
+
+    ServerResponse<PageInfo> list(Integer userId, int pageNum, int pageSize);
+}
